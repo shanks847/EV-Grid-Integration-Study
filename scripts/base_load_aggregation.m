b22431e5ef75b9bcebbcb92c07ec54fa3b5ecc59
Shankar Ramharack <shankar.ramharack@gmail.com>
2022-11-12 19:55:34 -0400

    Added Base Load Aggregation Code

---
+++ scripts/base_load_aggregation.m
@@ -0,0 +1,15 @@
+function [aggregated_customer_load] = base_load_aggregation(customer_base_load,charging_level,start_time,duration)
+%BASE_LOAD_AGGREGATION Adds EV Charging Load to Residential Base Load
+%   This function computes the total residential load from EV Charge
+%   contributions. It does this by determinig the total power output
+%   from a charging event based on the power rating at the level, start
+%   time and duration of the charging event.
+charger_power = 0
+if charging_level == 1
+    charger_power = 1920
+else
+    charger_power = 6600
+
+ev_load = 0;
+aggregated_customer_load = customer_base_load + ev_load;
+end
