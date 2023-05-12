94315788cdedd0fb843d67e6ebaca5a163a4b9d4
Shankar Ramharack <shankar.ramharack@gmail.com>
2023-05-12 14:51:10 -0400

    Recovered data lost through corruption and using balanced base load

--- scripts/base_load_aggregation.m
+++ scripts/base_load_aggregation.m
@@ -1,3 +1,4 @@
+%function [aggregated_customer_load,load_profiles] = base_load_aggregation(customer_base_load,charging_level,start_time,duration)
 function [event_details,modified_cutils] = base_load_aggregation(start_distribution,duration_distribution, scenario_clevel, customer_ID, cutilres_tt)
 %BASE_LOAD_AGGREGATION  Adds power contribution from a randomized charging
 %event to the base load of a customer
@@ -22,17 +23,22 @@ end
 %make a charging scenario
 sst = start_distribution.random(); %generating scenario start time from start time distribution
 sp = duration_distribution.random(); %generating scenario duration from duration distribution
+
 scenario_end_time = minutes(sst) + minutes(sp);
 charging_mask = timerange(minutes(sst),scenario_end_time); %creating mask to isolate period of charging so there values can be modified
-
 
 modified_cutils = cutilres_tt;
 
 %make a charging scenario
 sst = 5*round(start_distribution.random()/5); %generating scenario start time from start time distribution
 sp = 5*round(duration_distribution.random()/5);
-while sp < 0
+while  (sp > 1440 || sp<0)
     sp = 5*round(duration_distribution.random()/5);
+    if (sp < 1440 && sp>0)
+        break
+    else
+        continue
+    end
 end
 scenario_end_time = minutes(sst) + minutes(sp);
 %rounding minutes to multiples of 5 to improve stability
@@ -40,12 +46,16 @@ charging_mask = timerange(minutes(sst),scenario_end_time); %creating mask to iso
 
 %import base load
 num_timestamps = size(modified_cutils(charging_mask,customer_ID),1);
-%disp("==================== Base Load Before (REDUCED TO AREA OF MASK) ====================")
-%modified_cutils(charging_mask,customer_ID)
+% disp("==================== Base Load Before (REDUCED TO AREA OF MASK) ====================")
+% modified_cutils(charging_mask,customer_ID)
 modified_cutils{charging_mask,[customer_ID]} = modified_cutils{charging_mask,[customer_ID]} + ones(num_timestamps,1)*charging_level;
-%disp("==================== Base Load After (REDUCED TO AREA OF MASK) ====================")
-%modified_cutils(charging_mask,customer_ID)
+% disp("==================== Base Load After (REDUCED TO AREA OF MASK) ====================")
+% modified_cutils(charging_mask,customer_ID)
 
 event_details = {customer_ID,scenario_clevel,minutes(sst),minutes(sp)};
+% disp("DISTRIBUTION SAMPLE")
+% sp
+% disp("COVERTED TO MINUTES")
+% sp
 
 end
